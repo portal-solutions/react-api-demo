261c8386d32b9ae4a6a516d7169911afe9d67d52
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2020-02-18 19:14:24 -0330

    Updates

---
+++ precache-manifest.63dcaf2a3abbf5a85f6e084593676445.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84eefa28438172cbcae01387627a4ebc",
+    "url": "/react-api-demo/index.html"
+  },
+  {
+    "revision": "b19bd4d610b2d084154c",
+    "url": "/react-api-demo/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b19bd4d610b2d084154c",
+    "url": "/react-api-demo/static/js/2.c4d4521d.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/react-api-demo/static/js/2.c4d4521d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1198025a5c433afebb0",
+    "url": "/react-api-demo/static/js/main.89063841.chunk.js"
+  },
+  {
+    "revision": "22d5c5913eeab83245ab",
+    "url": "/react-api-demo/static/js/runtime-main.038ffa65.js"
+  }
+]);
